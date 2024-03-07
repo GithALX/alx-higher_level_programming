c21f3dce4c0377bf052ce23aebe393f3f83fef56
Souhail Ouichou <ouichoueducational@gmail.com>
2024-03-07 10:01:02 -0800

    this is the mandatory tasks of 0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
+    if not isinstance(roman_string, str) or not roman_string:
+        return "not a string"
+    
     roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_n = 0
     for j in range(len(roman_string)):
@@ -11,3 +13,4 @@ def roman_to_int(roman_string):
         else:
             roman_n += roman_d[roman_string[j]]
     return roman_n
+
