3fd2f7f6316d5357aece65cedf2e3eadf7748acb
Souhail Ouichou <ouichueducational@gmail.com>
2024-09-26 13:10:37 -0700

    Fixed the .format errors instead of f-strings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+s = "and is less than 6 and not 0"
+if number < 0:
+    num = -number
+    n_rem = num % 10
 rem = number % 10
 if rem == 0:
     print(f"Last digit of {number:d} is {rem:d} and is {0}")
 elif rem > 5:
-    print(f"Last digit of {number:d} is {rem:d} and is greater than {5}")
+    if number < 0:
+        print(f"Last digit of {number:d} is -{n_rem:d} {s}")
+    else:
+        print(f"Last digit of {number:d} is {rem:d} and is greater than {5}")
 elif rem < 6:
-    print(f"Last digit of {number:d} is {rem:d} and is less than {6} and not {0}")
+    if number < 0:
+        print(f"Last digit of {number:d} is -{n_rem:d} {s}")
+    else:
+        print(f"Last digit of {number:d} is {rem:d} {s}")
