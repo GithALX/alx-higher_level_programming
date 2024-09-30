7ed2aa3beb281fd0e01b2b6d09e04c9feae6d909
Souhail Ouichou <ouichueducational@gmail.com>
2024-09-30 13:34:11 -0700

    fixed the code so it wouldn't be executed when imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,10 @@
 import sys
 argv = sys.argv
 result = 0
-if len(argv) == 1:
-    print("{}".format(result))
-else:
-    for i in range(1, len(argv)):
-        result += int(argv[i])
-    print("{}".format(result))
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("{}".format(result))
+    else:
+        for i in range(1, len(argv)):
+            result += int(argv[i])
+        print("{}".format(result))
